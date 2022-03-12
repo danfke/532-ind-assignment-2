b9920e8496ad31351763d5b36b7c5595f3c08059
Daniel King <dnlkng13@gmail.com>
2022-03-11 18:14:00 -0800

    Prepare for heroku deployment

---
+++ init.R
@@ -0,0 +1,28 @@
+# R script to run author supplied code, typically used to install additional R packages
+# contains placeholders which are inserted by the compile script
+# NOTE: this script is executed in the chroot context; check paths!
+
+r <- getOption('repos')
+r['CRAN'] <- 'http://cloud.r-project.org'
+options(repos=r)
+
+# ======================================================================
+
+# packages go here
+install.packages(
+  c(
+    'dash',
+    'readr',
+    'here',
+    'ggthemes',
+    'remotes',
+    'lubridate',
+    'ggplot2',
+    'plotly',
+    'stringr',
+    'dplyr',
+    'tidyr',
+    'scales'
+  )
+)
+remotes::install_github('facultyai/dash-bootstrap-components@r-release')
